59f6cd0a9f6e964f0d90ea8d7f1b1295f6c363b2
codeuiandy <frostandy41@gmail.com>
2021-12-10 16:57:25 +0100

    First push

---
+++ src/components/screens/home/index.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { baseUrl, httpGet } from "../../utils/https";
+import "./index.css";
+const Network = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getNetworks();
+  }, []);
+
+  const getNetworks = async () => {
+    const res = await httpGet("api/v1/chains/properties");
+    if (res.er) {
+      return alert("Error found");
+    }
+    let convertObjectToArray = Object.values(res);
+    let filterArray = convertObjectToArray.filter((data) => {
+      return data.tokenSymbol && data.tokenDecimals;
+    });
+    console.log(filterArray);
+    setData(convertObjectToArray);
+    setLoading(false);
+  };
+
+  const getStatus = async (name) => {
+    const res = await httpGet(`api/v1/check/${name}`);
+    if (res.er) {
+      return "Not found";
+    }
+    return res;
+  };
+
+  const MapData = () => {
+    return loading ? null : (
+      <div className="connections">
+        {data.map((res, index) => {
+          console.log(res.name);
+          return (
+            <div key={index} className="connect">
+              <img src={`${baseUrl}/icons/${res?.icon}`} alt="" />
+              <h2>{res?.name}</h2>
+
+              {getStatus(res.name) == true ? (
+                <div className="status-container">
+                  <h3>Disconnected</h3>
+                  <div className="status-rect Connected"></div>
+                </div>
+              ) : (
+                <div className="status-container">
+                  <h3>Disconnected</h3>
+                  <div className="status-rect disconnected"></div>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  return (
+    <div>
+      <div className="app-header-wrap">
+        <h1 className="app-header">Connections</h1>
+      </div>
+      {loading ? <p className="app-loading">Loading...</p> : <MapData />}
+    </div>
+  );
+};
+
+export default Network;
---
+++ src/components/utils/https.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+export const baseUrl = "https://app.subsocial.network/subid/";
+// export let baseUrl = process.env.REACT_APP_BASE_URL;
+
+export const httpGet = async (url) => {
+  if (!navigator.onLine) {
+    return alert("Please check your internet", "Opps!", 3000);
+  }
+  try {
+    const res = await axios.get(`${baseUrl}${url}`, {
+      headers: { Authorization: `Bearer ${localStorage.token}` },
+    });
+    return res.data;
+  } catch (error) {
+    return { er: error.response.data };
+  }
+};
+
+export const httpPost = async (url, postBody) => {
+  if (!navigator.onLine) {
+    return alert("Please check your internet", "Opps!", 3000);
+  }
+  try {
+    const res = await axios.post(`${baseUrl}${url}`, postBody, {
+      headers: { Authorization: `Bearer ${localStorage.token}` },
+    });
+    return res.data;
+  } catch (error) {
+    return { er: error.response.data };
+  }
+};
